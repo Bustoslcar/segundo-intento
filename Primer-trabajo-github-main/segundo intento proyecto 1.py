706f7d8d190c3f50f4026f2d2ca2e5483f225a41
Bustoslcar <bustoslcar@gmail.com>
2022-07-04 04:39:45 -0300

    Entrar

--- Primer-trabajo-github-main/segundo intento proyecto 1.py
+++ Primer-trabajo-github-main/segundo intento proyecto 1.py
@@ -1,7 +1,7 @@
 #Autor: nd
 #Fecha: 29/06/2022
 #Descripcion: Prueba github
-print ("integrantes del grupo")
+print ("Integrantes del grupo de modalidad SCRUM")
 print ("Micaela Bustos")
 print ("Javier Altamirano")
 print("Margarita Bravo")
@@ -10,35 +10,37 @@ print("Damian Bravo ")
 print ("Daniel Varela")
 print("sofia bravo")
 
-print ("vamos a hacer una suma")
-print("ingrese el primer número: ")
+print ("vamos a hacer una SUMA DE 3 NUMEROS")
+print ()
+print("Ingrese el primer número: ")
 n1= float(input())
-print("ingrese el segundo número: ")
+print("Ingrese el segundo número: ")
 n2= float(input())
-print ("ingrese el tercer número: ")
+print ("Ingrese el tercer número: ")
 n3= float(input())
 suma= n1 + n2 + n3
 print("La suma es: ", suma)
-
-print ("vamos a hacer una resta")
-print("ingrese el primer número: ")
+ 
+ print()
+print ("vamos a hacer una RESTA DE 2 NUMEROS")
+print("Ingrese el primer número: ")
 n1= float(input())
-print("ingrese el segundo número: ")
+print("Ingrese el segundo número: ")
 n2= float(input())
 resta= n1 - n2
 print("La resta es: ", resta)
 
-
-print ("vamos a hacer una multiplicacion")
-print("ingrese el primer número: ")
+print()
+print ("vamos a hacer una MULTIPLICACIÓN DE 4 NUMEROS")
+print("Ingrese el primer número: ")
 n1= float(input())
-print("ingrese el segundo número: ")
+print("Ingrese el segundo número: ")
 n2= float(input())
-print("ingrese el segundo número: ")
+print("Ingrese el segundo número: ")
 n3= float(input())
-print("ingrese el segundo número: ")
+print("Ingrese el segundo número: ")
 n4= float(input())
-print("ingrese el segundo número: ")
+print("Ingrese el segundo número: ")
 print()
 multi= n1*n2*n3*n4
 print("La multiplicacion es: ", multi)
